a24ece96f18d34f1d5a7606de3836c194049f965
Chris Droom <github@droom.io>
2024-06-04 17:45:57 +0100

    init

---
+++ js/mouseflow.js
@@ -0,0 +1,7 @@
+var _mfq = _mfq || [];
+(function() {
+    var mf = document.createElement("script");
+    mf.type = "text/javascript"; mf.async = true;
+    mf.src = "//cdn.mouseflow.com/projects/4b741a92-c959-4055-878c-cf498ee60cf9.js";
+    document.getElementsByTagName("head")[0].appendChild(mf);
+})();
